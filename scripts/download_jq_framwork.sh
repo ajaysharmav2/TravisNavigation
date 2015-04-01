67d1c0e22ffb2365bc09afebe841035862572764
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-01 22:13:32 +0530

    Changes in JQ FM path

--- scripts/download_jq_framwork.sh
+++ scripts/download_jq_framwork.sh
@@ -9,7 +9,10 @@ export JQ_OUTPUTDIR="$PWD"
 export PERSONAL_API_KEY="6c1b9810a91801321dcc22000ac44c54"
 # Downloading JSON Framework
 echo "Downloading JSON Framework \n"
-curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/download/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/linux64/jq' -o 'jq' -L
+#curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/download/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/linux64/jq' -o 'jq' -L
+
+
+curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/download/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/linux64/jq' -o '$JQ_OUTPUTDIR/jq' -L
 
 # Chaning permission of JSON Framework
 echo "Chaning permission of JSON Framework \n"
