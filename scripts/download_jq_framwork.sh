21f6b6083c27810eb821ffa363edebf629ca5b27
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-01 21:00:45 +0530

    Changes in JQ FM path

--- scripts/download_jq_framwork.sh
+++ scripts/download_jq_framwork.sh
@@ -10,21 +10,11 @@ echo "Downloading JSON Framework \n"
 curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/download/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/linux64/jq' -o 'jq' -L
 
 # Chaning permission of JSON Framework
+echo "Chaning permission of JSON Framework \n"
+chmod +x ./jq
 
 # Moving JSON Framework
 echo "Moving JSON Framework \n"
-#sudo cp ./jq /usr/local/bin
-
-echo "Nitesh Working directory \n"
-
-pwd
-
-#mkdir /home/jqparser
-
-sudo cp ./jq pwd
+sudo cp ./jq /usr/bin/
 
 
-
-
-echo "Chaning permission of JSON Framework \n"
-chmod +x pwd/jq
