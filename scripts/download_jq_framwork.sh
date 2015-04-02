3fd94178b3c3b210f68a27d9ce6531402677e5b1
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-02 11:48:07 +0530

    Added ls befroe and after

--- scripts/download_jq_framwork.sh
+++ scripts/download_jq_framwork.sh
@@ -8,7 +8,18 @@ export JQ_OUTPUTDIR="$PWD"
 
 export PERSONAL_API_KEY="6c1b9810a91801321dcc22000ac44c54"
 # Downloading JSON Framework
+
+echo "Nitesh List of Directory \n "
+
+
+echo "************************************ "
+ls
+
+echo "************************************ "
+
 echo "Downloading JSON Framework \n"
+
+
 #curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/download/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/linux64/jq' -o 'jq' -L
 
 
