8ca02082283871b784bb14f86c163ad9ca6fefc2
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-06 22:23:56 +0530

    Changes in plist, changes in script to store output in variable

--- scripts/Get_All_Pivotal_Comments.sh
+++ scripts/Get_All_Pivotal_Comments.sh
@@ -23,51 +23,30 @@ if [ ! -z "$INFO_PLIST" ]; then
 echo "Set CFBundleVersion to $LABEL_ID"
 fi
 
-echo "JQ Path"
-echo $JQ_OUTPUTDIR
+curl -X GET -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories?with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
 
 
-echo "PWD - GetAll Script"
-
-#echo PWD
+#cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] .comments [].text' > $PWD/$ALL_COMMENTS
 #
-#echo $PWD
-
-curl -X GET -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories?date_format=millis&fields=current_state%2Ccomments&with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
-
-#chmod +x $OUTPUT_FILE_NAME
-
-echo "Show all the comments \n"
-
-#cat $PWD/$OUTPUT_FILE_NAME 
-
-#echo "Parse text file"
-
+#
+#chmod +x $PWD/$ALL_COMMENTS
 #ls -la
-#
-#echo "type 1"
-cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] .comments [].text' > $ALL_COMMENTS
-
-
-chmod +x $PWD/$ALL_COMMENTS
-
-while read line
-do
-ALL_NOTES=$ALL_NOTES$LINE$line
-#echo $line
-done <$PWD/ALL_COMMENTS
-echo "\n\nAll Text"
-echo $ALL_NOTES
-
-#cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] .comments [].text' > $ALL_COMMENTS
-
-
-
-
-#cat $ALL_COMMENTS | awk -F "[" '{print $2}'
-
-#$ALL_NOTES=($ALL_COMMENTS | awk -F "[" '{print $2}')
-#echo "All Filtered Commands"
-#
+#while read line
+#do
+#ALL_NOTES=$ALL_NOTES$LINE$line
+##echo $line
+#done <$PWD/ALL_COMMENTS
+#echo "\n\nAll Text"
 #echo $ALL_NOTES
+
+#cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] .comments [].text'
+
+echco "Local Variable "
+value=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
+echo "$value"
+
+echo "Global Variable"
+$ALL_NOTES=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
+echo "$ALL_NOTES"
+
 
