4341fc4f00cba25cce7288c62f25bd2b44c2d0be
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-08 22:45:32 +0530

    QA Branch Changes

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -16,6 +16,10 @@ if [[ "$TRAVIS_BRANCH" != "master" ]]; then
 echo "Testing on a branch other than master. No deployment will be done."
 exit 0
 fi
+if [[ "$TRAVIS_BRANCH" != "QA" ]]; then
+echo "Testing on a branch other than master. No deployment will be done."
+exit 0
+fi
 
 # Thanks @djacobs https://gist.github.com/djacobs/2411095
 # Thanks @johanneswuerbach https://gist.github.com/johanneswuerbach/5559514
