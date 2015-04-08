bbf254960ebf5ff0a17af7d5273c8db340a064de
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-08 21:36:15 +0530

    Reset old code

--- scripts/Get_All_Pivotal_Comments.sh
+++ scripts/Get_All_Pivotal_Comments.sh
@@ -7,9 +7,16 @@
 #  Copyright (c) 2014 ___v2Tech Ventures___. All rights reserved.
 #!/bin/sh
 
-export LABEL_ID=''
+
+
+#export PROJECT_ID=1114232
+#export TOKEN='cf9391f2af5a2b68da2755a27bce2bff'
+#export LABEL_ID='b199'
 export OUTPUT_FILE_NAME='jsonrespone.text'
+
 export ALL_COMMENTS='allcomments.text'
+
+export LABEL_ID=''
 
 echo "PATH info plist"
 echo $INFOPLIST_FILE
@@ -17,14 +24,13 @@ echo $INFOPLIST_FILE
 
 # 'b is label prefix coded as standard prefix for all project'
 if [ ! -z "$INFOPLIST_FILE" ]; then
-   LABEL_ID=b`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" $INFOPLIST_FILE`
+LABEL_ID=b`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" $INFOPLIST_FILE`
 
 fi
+#build/Release-iphoneos/AppNavigation.app/Info.plist
 echo $LABEL_ID
 echo LABEL_ID
 
 curl -X GET -H "X-TrackerToken: $PIVOTAL_TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PIVOTAL_PROJECT_ID/stories?with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
-chmod +x $OUTPUT_FILE_NAME
 
-chmod +x $ALL_COMMENTS_FILE
 
--- scripts/download_jq_framwork.sh
+++ scripts/download_jq_framwork.sh
@@ -1,16 +1,11 @@
 
-#!/bin/sh
-
-#  Get_All_Pivotal_Comments.sh
+#export PROJECT_ID=1114232
+#export TOKEN='cf9391f2af5a2b68da2755a27bce2bff'
 #
-#
-#  Created by Nitesh Meshram on 16/03/15.
-#  Copyright (c) 2014 ___v2Tech Ventures___. All rights reserved.
-#!/bin/sh
-
+#curl -X GET -H "X-TrackerToken: cf9391f2af5a2b68da2755a27bce2bff" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/1114232/stories?date_format=millis&with_label=b104"
 
 export JQ_OUTPUTDIR="$PWD"
-#
+
 #export PERSONAL_API_KEY="6c1b9810a91801321dcc22000ac44c54"
 
 curl --header 'Host: stedolan.github.io' --header 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' --header 'Accept-Language: en-US,en;q=0.5' --header 'Referer: http://stedolan.github.io/jq/' --header 'Connection: keep-alive' 'http://stedolan.github.io/jq/download/osx64/jq' -o $JQ_OUTPUTDIR/jq
@@ -21,5 +16,13 @@ echo $JQ_OUTPUTDIR
 # Chaning permission of JSON Framework
 echo "Chaning permission of JSON Framework \n"
 chmod +x $JQ_OUTPUTDIR/jq
+#chmod 777 $JQ_OUTPUTDIR/jq
+
+ls -la
+#
+#
+## Moving JSON Framework
+#echo "Moving JSON Framework \n"
+#sudo cp $JQ_OUTPUTDIR/jq /usr/local/bin/
 #
 
