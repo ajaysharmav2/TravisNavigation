1bcc1eef77340a9568a66a5ca756814bebe568ec
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-08 17:45:04 +0530

    Enable Hockey app uploading

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -44,88 +44,30 @@ echo $ALL_NOTES
 
 RELEASE_NOTES="$ALL_NOTES\nTravis Integration Build: $TRAVIS_BUILD_NUMBER"
 
-#ALL_NOTES=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | .id, .name' | awk -F '\n' {print $1, $2}`
-# Get the release notes
-#- ./scripts/Get_All_Pivotal_Comments.sh
-#sh ./scripts/Get_All_Pivotal_Comments.sh
 
-#if [ ! -z "$TESTFLIGHT_TEAM_TOKEN" ] && [ ! -z "$TESTFLIGHT_API_TOKEN" ]; then
-#  echo ""
-#  echo "***************************"
-#  echo "* Uploading to Testflight *"
-#  echo "***************************"
-#  curl http://testflightapp.com/api/builds.json \
-#    -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-#    -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
-#    -F api_token="$TESTFLIGHT_API_TOKEN" \
-#    -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
-#    -F distribution_lists='Internal' \
-#    -F notes="$RELEASE_NOTES"
-# echo "Upload finished"
-#fi
 
 HOCKEY_APP_ID="a3cd182b137aed43d6f092c0b259544a"
 HOCKEY_APP_TOKEN="759fb41a1d2e440e98d10d0cb022cbe2"
 echo "*        Testing for HockeyApp          *"
 
-#if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
-#echo ""
-#echo "***************************"
-#echo "* Uploading to Hockeyapp  *"
-#echo "***************************"
-#curl  \
-#-F "status=2" \
-#-F "notify=0" \
-#-F "notes=$RELEASE_NOTES" \
-#-F "notes_type=0" \
-#-F "ipa=@$OUTPUTDIR/$APP_NAME.ipa" \
-#-F "dsym=@$OUTPUTDIR/$APP_NAME.app.dsym.zip" \
-#-H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
-#https://rink.hockeyapp.net/api/2/apps/upload
-#echo "Upload finish"
-#fi
-
-#{"title":"Travis Test","bundle_identifier":"com.travis.travisnavigation","public_identifier":"a3cd182b137aed43d6f092c0b259544a","platform":"iOS","release_type":0,"custom_release_type":null,"created_at":"2015-03-02T09:11:57Z","updated_at":"2015-03-09T14:48:23Z","featured":false,"id":150444,"config_url":"https://rink.hockeyapp.net/manage/apps/150444/app_versions/2","public_url":"https://rink.hockeyapp.net/apps/a3cd182b137aed43d6f092c0b259544a","minimum_os_version":"7.0","device_family":"iPhone/iPod","status":2,"owner":"Backup Data","owner_token":"368b1383c024a45f61476ee1371b26494137c339"}
-
-#PERSONAL_API_KEY="6c1b9810a91801321dcc22000ac44c54"
-
-#echo "Global Variable - 1"
-#$ALL_NOTES=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
-#
-#MYNOTE=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
-
-#RELEASE_TITLE="UberTestersDemo4.0"
-#
-#
+if [ ! -z "$HOCKEY_APP_ID" ] && [ ! -z "$HOCKEY_APP_TOKEN" ]; then
+echo ""
 echo "***************************"
-echo "* Uploading to UberTesters  *"
-echo $ALL_NOTES
+echo "* Uploading to Hockeyapp  *"
 echo "***************************"
-echo ALL_NOTES
-
-curl  http://beta.ubertesters.com/api/client/upload_build.json \
--F file="@$OUTPUTDIR/$APP_NAME.ipa" \
--F title="$RELEASE_TITLE" \
--F notes="$RELEASE_NOTES" \
--F status="in_progress" \
--F stop_previous="true" \
--H "X-UbertestersApiKey:$PERSONAL_API_KEY" \
-
-echo "*****  Upload finish *****"
-
-
-# Script for Getting All Comments
-#- ./scripts/Get_All_Pivotal_Comments.sh
+curl  \
+-F "status=2" \
+-F "notify=0" \
+-F "notes=$RELEASE_NOTES" \
+-F "notes_type=0" \
+-F "ipa=@$OUTPUTDIR/$APP_NAME.ipa" \
+-F "dsym=@$OUTPUTDIR/$APP_NAME.app.dsym.zip" \
+-H "X-HockeyAppToken: $HOCKEY_APP_TOKEN" \
+https://rink.hockeyapp.net/api/2/apps/upload
+echo "Upload finish"
+fi
 
 
 
-#echo "***************************"
-#echo "* Uploading to UberTesters  *"
-#echo "***************************"
-#curl http://beta.ubertesters.com/api/client/upload_build.json \
-#-F file="@$OUTPUTDIR/$APP_NAME.ipa" \
-#-H "X-UbertestersApiKey:$PERSONAL_API_KEY" \
-#echo "Upload finish"
-#{"success":true,"revision":{"platform":"iOS","application_id":"com.travis.travisnavigation","build_number":"2.0","created_at":"2015-03-10T09:31:14Z","title":"UberTestersDemo2.0","notes":"Build: 29\\nUploaded: 2015-03-10 09:22:00","size":1986804,"sdk_version":70,"min_os_version":"7.0","target_os_version":null,"project":"V2Demo Ubertesters / DemoUbertesters","status":"In progress","started_at":"2015-03-10T09:31:14Z","notify":true,"distribution_groups":[],"stop_previous":true}}Upload finish
 
 
