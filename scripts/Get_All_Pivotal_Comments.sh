ae2c026c63b53f22e032b9918b213a6a92bab345
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-08 15:11:22 +0530

    Changes in script for getting versions

--- scripts/Get_All_Pivotal_Comments.sh
+++ scripts/Get_All_Pivotal_Comments.sh
@@ -15,41 +15,19 @@ export TOKEN='cf9391f2af5a2b68da2755a27bce2bff'
 export OUTPUT_FILE_NAME='jsonrespone.text'
 
 export ALL_COMMENTS='allcomments.text'
+export LABEL_ID=''
 
 echo "PATH info plist"
-echo $PWD/$INFOPLIST_FILE
+echo $INFO_PLIST
 
-LABEL_ID=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$PWD/$INFOPLIST_FILE")
-echo "MY App Version"
-echo $LABEL_ID
 
+# 'b is label prefix coded as standard prefix for all project'
 if [ ! -z "$INFO_PLIST" ]; then
-LABEL_ID=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$INFO_PLIST"
-#echo "Set CFBundleVersion to $TRAVIS_BUILD_NUMBER"
+   LABEL_ID=b`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" $INFO_PLIST`
+
 fi
 
-
-#export LINE="\n"
-#if [ ! -z "$INFO_PLIST" ]; then
-#/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $LABEL_ID" "$INFO_PLIST"
-#echo "Set CFBundleVersion to $LABEL_ID"
-#fi
-
-
-
+echo $LABEL_ID
 
 curl -X GET -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories?with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
-
-#echo "Local Variable "
-#value=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
-#echo "$value"
-
-#echo "Global Variable - 1"
-#export ALL_NOTES=`cat $PWD/$OUTPUT_FILE_NAME | $PWD/jq '.[] | {name: .name, id: .id}'`
-
-
-#echo "$ALL_NOTES"
-
-#echo "Global Variable - 2"
-#echo $ALL_NOTES
 
