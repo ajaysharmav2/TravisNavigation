c6030afcec8c8ff1269b04cd0a6d8808cb5ba6cc
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-04-09 12:36:05 +0530

    QA - Rename variabls

--- scripts/Get_All_Pivotal_Comments.sh
+++ scripts/Get_All_Pivotal_Comments.sh
@@ -33,6 +33,6 @@ fi
 echo $LABEL_ID
 echo LABEL_ID
 
-curl -X GET -H "X-TrackerToken: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories?with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
+curl -X GET -H "X-TrackerToken: $PIVOTAL_TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" "https://www.pivotaltracker.com/services/v5/projects/$PIVOTAL_PROJECT_ID/stories?with_label=$LABEL_ID" -o $PWD/$OUTPUT_FILE_NAME
 
 
